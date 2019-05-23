3ed8deff6da993f2149f1c2a47c9f40242089925
devops2 <akshay.bharambe@mohartech.com>
2019-05-23 14:55:24 +0530

    Project Deleted for New Updated changes

--- src/app/shared/custom-option.ts
+++
@@ -1,9 +0,0 @@
-import {ToastOptions} from 'ng6-toastr';
-
-export class CustomOption extends ToastOptions {
-  animate = 'fade'; // you can override any options available
-  newestOnTop = false;
-  showCloseButton = true;
-  positionClass = 'toast-top-center';
-  toastLife = 6000;
-}
--- src/environments/environment.uat.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-   apiUrl: 'http://114.143.244.195:8067/api/',
-   apiUrlNot: 'http://192.168.1.116:8002/api/',
-   l2apiUrl: 'http://114.143.244.195:8067/l2api/',
-  pdfUrl: 'https://s3-ap-southeast-1.amazonaws.com/maacpd/static/pdf/',
-  redirectUrl:'http://114.143.244.195:8067/#/l1/'
-};
