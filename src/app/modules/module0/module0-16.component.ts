bbbddf25da56d0d8b43319e938f15f3efdee07d1
pravin <pravinp1314@gmail.com>
2022-12-14 17:49:39 +0530

    module 0 submodule0.16 added

--- src/app/modules/module0/module0-16.component.ts
+++ src/app/modules/module0/module0-16.component.ts
@@ -5,7 +5,7 @@ import { LanguageService } from './../../language.service';
 import { Router } from '@angular/router';
 import { ToastsManager } from 'ng6-toastr';
 import { TranslateService } from '@ngx-translate/core';
-import {Module0Service} from './module0.service'
+import { Module0Service } from './module0.service'
 
 @Component({
   selector: 'app-module0-16',
@@ -22,10 +22,13 @@ export class Module016Component implements OnInit {
   )
   showVideoFlag: boolean;
   passUrl: any;
+  videoflag = 0;
+  passValues={};
+  showCFU: boolean;
   constructor(
     public LanguageService: LanguageService,
     private LocalstoragedetailsService: LocalstoragedetailsService,
-    private router: Router,     
+    private router: Router,
     public Module0Service: Module0Service,
     public toastr: ToastsManager,
     vcr: ViewContainerRef,
@@ -33,41 +36,47 @@ export class Module016Component implements OnInit {
   ) {
     this.toastr.setRootViewContainerRef(vcr);
   }
-  public passData = {}; 
+  public passData = {};
   ngOnInit() {
-    this.start(); 
+    this.start();
   }
   start() {
     var jsonBody = {}
     jsonBody['submoduleid'] = window.localStorage.getItem('uuid')
     jsonBody['event'] = 'start'
-    this.apiCall(jsonBody, 'modulezerosingleurl/', 'start');   
+    this.apiCall(jsonBody, 'modulezerosingleurl/', 'start', 0);
   }
-  videoFinish(e) {
-    if (e == true)
-    {
+  videoFinish(e, item) {
+    this.videoflag = item
+    if (e == true) {
       this.instructionModal.show()
       this.LanguageService.toShow();
       // this.nextBtnFlag = true
     }
   }
-  next() {
+  next(item) {
     var jsonBody = {}
     jsonBody['submoduleid'] = window.localStorage.getItem('uuid')
     jsonBody['event'] = 'finish'
-    this.apiCall(jsonBody, 'modulezerosingleurl/', 'finish1')
+    if (item == 5) {
+
+      this.apiCall(jsonBody, 'modulezerosingleurl/', 'finish1', item)
+    } else {
+      this.apiCall(jsonBody, 'modulezerosingleurl/', 'finish1', item)
+    }
+
   }
 
-  apiCall(jsonBody, apiUrl, fun) {
+  apiCall(jsonBody, apiUrl, fun, item) {
     this.Module0Service.apiCall(jsonBody, apiUrl).subscribe(
       data => {
         if (data["status"] == true) {
           if (fun == "start") {
             this.LanguageService.googleEventTrack('L3SubmoduleStatus', 'Module 0.15', window.localStorage.getItem('username'), 10);
-
+            this.showCFU = false;
+            this.passValues['url'] = data['data'].url;
             this.passData['apiUrl'] = "modulezerosingleurl/";
             this.passData['videoUrl'] = data['data'].url;
-            console.log("sacsac", this.passData)
             this.showVideoFlag = true
             this.passUrl = data['data'].url;
             var current0 = [];
@@ -78,15 +87,22 @@ export class Module016Component implements OnInit {
 
             window.localStorage.setItem("currentJson0", JSON.stringify(current0));
           } else if (fun == "finish1") {
-            this.instructionModal.hide();
-            this.LanguageService.toHide();
-            window.localStorage.setItem('uuid', data['data'].nextuuid)
-            window.localStorage.setItem('mainFlagModule0', '16');
-            window.localStorage.setItem('subFlagModule0', '1');
-            window.localStorage.setItem('source', 'module 0.16');
-            this.Module0Service.setLocalStorage0(3);
-            var obj = { "type": "submodule", "route": true, "current": this.translate.instant('L2Module0.subMenu0-6'), "next": this.translate.instant('L2Module0Finish.subMenu0-15'), "nextRoute": "/modules/module0/Module0.16" }
-            this.LocalstoragedetailsService.setModuleStatus(JSON.stringify(obj));
+
+            if (item == 5) {
+              this.instructionModal.hide();
+              this.LanguageService.toHide();
+              window.localStorage.setItem('uuid', data['data'].nextuuid)
+              window.localStorage.setItem('mainFlagModule0', '17');
+              window.localStorage.setItem('subFlagModule0', '1');
+              window.localStorage.setItem('source', 'module 0.17');
+              this.Module0Service.setLocalStorage0(3);
+              var obj = { "type": "submodule", "route": true, "current": this.translate.instant('L2Module0.subMenu0-17'), "next": this.translate.instant('L2Module0Finish.subMenu0-17'), "nextRoute": "/modules/module0/Module0.17" }
+              this.LocalstoragedetailsService.setModuleStatus(JSON.stringify(obj));
+            }else{
+              this.instructionModal.hide();
+              this.LanguageService.toHide();
+            }
+
           }
         }
       },
@@ -94,5 +110,46 @@ export class Module016Component implements OnInit {
         this.LanguageService.handleError(error.error.message);
       }
     );
+  }
+
+
+  nextvideo(item) {
+    this.subFlagModule0 = this.subFlagModule0 + 1
+    window.localStorage.setItem('subFlagModule0', this.subFlagModule0.toString());
+    this.instructionModal.hide();
+    var jsonBody = {}
+    jsonBody['submoduleid'] = window.localStorage.getItem('uuid')
+    jsonBody['event'] = 'finish'
+    if (item == 5) {
+      this.apiCall(jsonBody, 'modulezerosingleurl/', 'finish1', item)
+      
+    } else {
+      this.nextApiCall(jsonBody, 'modulezerosingleurl/', 'finish1')
+    }
+   
+  }
+
+  nextApiCall(jsonBody, apiUrl, fun) {
+    this.Module0Service.apiCall(jsonBody, apiUrl).subscribe(
+      data => {
+        if (data["status"] == true) {
+          if (fun == "finish1") {
+            window.localStorage.setItem('uuid', data['data'].nextuuid)
+            this.start()
+          }
+        }
+      },
+      error => {
+        this.LanguageService.handleError(error.error.message);
+      }
+    );
+  }
+
+  finishPDF(e, item) {
+    this.subFlagModule0 = this.subFlagModule0 + 1
+    var jsonBody = {}
+    jsonBody['submoduleid'] = window.localStorage.getItem('uuid')
+    jsonBody['event'] = 'finish'
+    this.nextApiCall(jsonBody, 'modulezerosingleurl/', 'finish1')
   }
 }
